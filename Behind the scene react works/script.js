11445e47f276ab5967289c0eb0af290692dc7e9e
Sheetu1 <sheetusharma548@gmail.com>
2025-04-15 03:05:15 +0530

    Behind the scene how works in react

---
+++ Behind the scene react works/script.js
@@ -0,0 +1,23 @@
+ // const heading = React.createElement("h1",{},"Hello world from react!");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+// parent inside child div than h1 tage inside this is h2 tag and h2 tag
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+     [ React.createElement("h1", {}, "This is heading tag"),React.createElement("h2",{},"This is ih2 tag")]
+    )
+  );
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  console.log(parent);
+  
+  root.render(parent);
+
+
+// 
