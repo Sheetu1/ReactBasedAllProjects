556ce78a09552f273e7e1be011bb34b96c52f08b
Sheetu1 <sheetusharma548@gmail.com>
2025-05-10 02:16:11 +0530

    basic ui done

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  let saveTodoList = (e) => {
+    e.preventDefault();
+    let ToName = e.target.ToName.value;
+    alert(ToName);
+  }
+
   return (
-    <div>App</div>
+    <div>
+     <form onSubmit={saveTodoList} className='flex items-center justify-center pt-8 gap-2'>
+      <h1>ToDo List</h1>
+       <input className='px-10 py-2  rounded-md ' type="text" name='ToName' placeholder='Enter Your Task' />
+      <button className='px-2 py-2 bg-blue-500 text-white rounded-md '>Add Task</button>
+     </form>
+
+    </div>
   )
 }
 
