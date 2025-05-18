74cf352ad282ecb754ccaab69c0506e0f68eb04e
Sheetu1 <sheetusharma548@gmail.com>
2025-05-19 03:54:29 +0530

    Immediately invoked

--- eventHandling/src/App.jsx
+++ eventHandling/src/App.jsx
@@ -1,29 +1,37 @@
 
 import React from "react";
 const App = () => {
-  const handleClick = () => {
-    alert("I am clicked");
-  }
+  // const handleClick = () => {
+  //   alert("I am clicked");
+  // }
 
-  const handlemouseOver = () => {
-    console.log("Yes i'm sheetal sharma..");
-  }
+  // const handlemouseOver = () => {
+  //   console.log("Yes i'm sheetal sharma..");
+  // }
 
-  const changeHandler = (e) => {
-    console.log("Me change ho rha hu");
-    console.log(e.target.value);
-  }
+  // const changeHandler = (e) => {
+  //   console.log("Me change ho rha hu");
+  //   console.log(e.target.value);
+  // }
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   alert("Form submit kr du kya");
+  // }
   return(
     <>
-    <div>
+    {/* <div>
       <button onClick={handleClick}>Click me</button>
     </div>
     <div>
       <h1 onMouseOver={handlemouseOver}>Hello sheetal sharma</h1>
     </div>
-    <form>
+    <form onSubmit={submitHandler}>
       <input onChange={changeHandler} type="text" />
-    </form>
+      <button>submit</button>
+    </form> */}
+
+    <button onClick={() => alert("i'm clicked")}> Click</button>
 
     </>
   )
