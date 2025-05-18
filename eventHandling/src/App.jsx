32aa09e8b9186494f454029cc8f22012f6b420e8
Sheetu1 <sheetusharma548@gmail.com>
2025-05-19 03:27:24 +0530

    Mouseover

--- eventHandling/src/App.jsx
+++ eventHandling/src/App.jsx
@@ -4,10 +4,19 @@ const App = () => {
   const handleClick = () => {
     alert("I am clicked");
   }
+  const handlemouseOver = () => {
+    console.log("Yes i'm sheetal sharma..");
+  }
   return(
+    <>
     <div>
       <button onClick={handleClick}>Click me</button>
     </div>
+    <div>
+      <h1 onMouseOver={handlemouseOver}>Hello sheetal sharma</h1>
+    </div>
+
+    </>
   )
 }
 export default App;
